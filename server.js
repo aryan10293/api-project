d29a119e352601332aecad279ac4adbec12396e2
aondrej ryan <aondrejryan@gmail.com>
2023-02-13 11:15:05 -0800

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 app.use(express.static('public'))
-const PORT = 5000
+app.use(cors())
+const PORT = process.env.PORT || 5000
 
 const rappers = {
     'drake': {
